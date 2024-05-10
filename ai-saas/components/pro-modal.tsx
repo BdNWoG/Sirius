c33c0b6e66799cda3afff458f2e5c83da9a5938d
BdNWoG <billy.gao@hotmail.com>
2024-05-10 09:32:49 -0700

    pro

--- ai-saas/components/pro-modal.tsx
+++ ai-saas/components/pro-modal.tsx
@@ -1,8 +1,47 @@
 "use client";
 
+import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
 import { useProModalStore } from "@/hooks/use-pro-modal";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "./ui/dialog";
 import { Badge } from "./ui/badge";
+
+const tools = [
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        color: "text-violet-500",
+        bgColor: "bg-violet-500/10",
+        href: "/conversation"
+    },
+    {
+        label: "Music Generation",
+        icon: Music,
+        color: "text-emerald-500",
+        bgColor: "bg-emerald-500/10",
+        href: "/music"
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        color: "text-pink-700",
+        bgColor: "bg-pink-700/10",
+        href: "/image"
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        color: "text-orange-700",
+        bgColor: "bg-orange-700/10",
+        href: "/video"
+    },
+    {
+        label: "Code Generation",
+        icon: Code,
+        color: "text-green-700",
+        bgColor: "bg-green-700/10",
+        href: "/code"
+    },
+];
 
 export const ProModal = () => {
     const proModal = useProModalStore();
@@ -14,11 +53,14 @@ export const ProModal = () => {
                     <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
                         <div className="flex items-center gap-x-2 font-bold py-1">
                             Upgrade Sirius
-                            <Badge className="uppercase text-sm py-1">
+                            <Badge variant="premium" className="uppercase text-sm py-1">
                                 Pro
                             </Badge>
                         </div>
                     </DialogTitle>
+                    <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
+
+                    </DialogDescription>
                 </DialogHeader>
             </DialogContent>
         </Dialog>
