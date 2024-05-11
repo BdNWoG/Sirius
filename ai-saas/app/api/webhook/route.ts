e7a8c01b2a1a48461a4c9a1d47649853cea1b3d4
BdNWoG <billy.gao@hotmail.com>
2024-05-10 22:54:30 -0700

    Create route.ts

---
+++ ai-saas/app/api/webhook/route.ts
@@ -0,0 +1,25 @@
+import Stripe from "stripe";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+
+export async function POST(req: Request) {
+    const body = await req.text();
+    const signature = headers().get("stripe-signature") as string;
+
+    let event: Stripe.Event;
+
+    try{
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET!
+        ); 
+    }
+    catch (error: any) {
+        console.log("[STRIPE_ERROR]", error);
+        return new NextResponse("Webhook Error: ${error.message}", { status: 400 });
+    }
+}
