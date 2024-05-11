67504ef92c9bd40f00d669ed0f250f435eb8f703
BdNWoG <billy.gao@hotmail.com>
2024-05-10 22:49:20 -0700

    stripe

--- ai-saas/components/navbar.tsx
+++ ai-saas/components/navbar.tsx
@@ -1,10 +1,13 @@
 import { UserButton } from "@clerk/nextjs"
 import MobileSidebar from "@/components/mobile-sidebar";
+import { getAPILimitCount } from "@/lib/api-limit";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const APILimitCount = await getAPILimitCount();
+
     return (
         <div className="flex items-center p-4">
-            <MobileSidebar/>
+            <MobileSidebar APILimitCount={APILimitCount}/>
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/"/>
             </div>
