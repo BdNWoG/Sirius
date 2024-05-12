dc76dfdbc786bf8c0b9816f09fa8400b9174915a
BdNWoG <billy.gao@hotmail.com>
2024-05-12 16:08:02 -0700

    settings

---
+++ ai-saas/app/(dashboard)/(routes)/settings/page.tsx
@@ -0,0 +1,20 @@
+import { Heading } from "@/components/heading"
+import { checkSubscription } from "@/lib/subscription";
+import { Settings } from "lucide-react";
+
+const SettingsPage = async () => {
+    const isPro = await checkSubscription();
+    return (
+        <div>
+            <Heading title="Settings" description="Manage your account settings." 
+            icon={Settings} iconColor="text-grey-700" bgColor="bg-grey-700/10"/>
+            <div className="px-4 lg:px-8 space-y-4">
+                <div className="text-muted-foreground text-sm">
+                    {isPro ? "You are currently on the Pro plan" : "You are currently on the Free plan"}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SettingsPage; 
