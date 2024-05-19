fc6a3e01512a2265dacf1d22f825475f45ff7876
Billy Gao <101796864+BdNWoG@users.noreply.github.com>
2024-05-18 19:46:38 -0700

    Update layout.tsx

--- ai-saas/app/layout.tsx
+++ ai-saas/app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Sirius",
   description: "AI platform",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
